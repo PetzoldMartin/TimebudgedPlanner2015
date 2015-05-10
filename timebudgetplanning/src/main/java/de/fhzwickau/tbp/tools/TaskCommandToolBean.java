fb8f640e610fa0a76a9d51654411415b2caf9f87
sis111su <sis111su@614dbce1-2ce3-4a13-8779-6e912a77b021>
2015-05-10 18:49:15 +0000

    generated code from updated model

---
+++ timebudgetplanning/src/main/java/de/fhzwickau/tbp/tools/TaskCommandToolBean.java
@@ -0,0 +1,47 @@
+package de.fhzwickau.tbp.tools;
+
+/* 
+ *	Do not place import/include statements above this comment, just below. 
+ * 	@FILE-ID : (_17_0_4_2_67b0227_1431276472150_541750_3490) 
+ */
+import de.fhzwickau.tbp.tools.dto.NewTask;
+import de.fhzwickau.tbp.tools.facade.TaskCommandTool;
+
+import javax.persistence.PersistenceContext;
+
+import javax.persistence.EntityManager;
+
+import javax.ejb.Stateless;
+
+/**
+ * Please describe the responsibility of your class in your modeling tool.
+ */
+
+@Stateless(name = "TaskCommandToolBean")
+public class TaskCommandToolBean implements TaskCommandTool {
+	
+	@PersistenceContext
+	private EntityManager entityManager;
+	
+	/**
+	 * Constructor for class '<em><b>TaskCommandToolBean</b></em>'.
+	 */
+	
+	public TaskCommandToolBean() {
+	}
+	
+	/**
+	 * Method stub for further implementation.
+	 */
+	
+	public void addTask(NewTask newTask) {
+		/* PROTECTED REGION ID(java.implementation._17_0_4_2_67b0227_1431276472150_541750_3490__17_0_4_2_67b0227_1431276272635_392480_3484) ENABLED START */
+		// TODO: implementation of method 'TaskCommandToolBean.addTask(...)'
+		throw new UnsupportedOperationException("The implementation of this generated method stub is missing!");
+		/* PROTECTED REGION END */
+	}
+	
+	/* PROTECTED REGION ID(java.class.own.code.implementation._17_0_4_2_67b0227_1431276472150_541750_3490) ENABLED START */
+	// TODO: put your own implementation code here
+	/* PROTECTED REGION END */
+}
---
+++ timebudgetplanning/src/main/java/de/fhzwickau/tbp/tools/facade/TaskCommandTool.java
@@ -0,0 +1,24 @@
+package de.fhzwickau.tbp.tools.facade;
+
+/* 
+ *	Do not place import/include statements above this comment, just below. 
+ * 	@FILE-ID : (_17_0_4_2_67b0227_1431276253958_640908_3465) 
+ */
+import de.fhzwickau.tbp.tools.dto.NewTask;
+
+/**
+ * Please describe the responsibility of your class in your modeling tool.
+ */
+
+public interface TaskCommandTool {
+	
+	/**
+	 * Method stub for further implementation.
+	 */
+	
+	public void addTask(NewTask newTask);
+	
+	/* PROTECTED REGION ID(java.interface.own.code.declaration._17_0_4_2_67b0227_1431276253958_640908_3465) ENABLED START */
+	// TODO: put your own implementation code here
+	/* PROTECTED REGION END */
+}
