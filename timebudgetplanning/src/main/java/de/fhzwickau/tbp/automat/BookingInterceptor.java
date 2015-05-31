8ed631d4d7d84371aa325982cc4dffabee5c6272
tos111j8 <tos111j8@614dbce1-2ce3-4a13-8779-6e912a77b021>
2015-05-31 08:23:40 +0000

    removed non-generated version of BookingInterceptor BookingInterceptor is now a version generated by GeneSEZ

--- timebudgetplanning/src/main/java/de/fhzwickau/tbp/automat/BookingInterceptor.java
+++ timebudgetplanning/src/main/java/de/fhzwickau/tbp/automat/BookingInterceptor.java
@@ -1,20 +1,64 @@
 package de.fhzwickau.tbp.automat;
 
-import java.lang.annotation.Retention;
-import java.lang.annotation.Target;
+/* 
+ *	Do not place import/include statements above this comment, just below. 
+ * 	@FILE-ID : (_17_0_4_2_8210263_1433001027643_848303_3640) 
+ */
+import javax.interceptor.InvocationContext;
+import javax.interceptor.AroundInvoke;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
-import javax.inject.Qualifier;
-import javax.interceptor.InterceptorBinding;
-import static java.lang.annotation.RetentionPolicy.RUNTIME;
-import static java.lang.annotation.ElementType.TYPE;
-import static java.lang.annotation.ElementType.METHOD;
-import static java.lang.annotation.ElementType.FIELD;
-import static java.lang.annotation.ElementType.PARAMETER;
+import de.fhzwickau.tbp.material.PlanningData;
+import de.fhzwickau.tbp.material.Project;
+import de.fhzwickau.tbp.material.Task;
+import de.fhzwickau.tbp.tools.dto.NewBooking;
 
-@Qualifier
-@InterceptorBinding
-@Retention(RUNTIME)
-@Target({TYPE, METHOD, FIELD, PARAMETER})
-public @interface BookingInterceptor {
+/**
+ * Please describe the responsibility of your class in your modeling tool.
+ */
 
+public class BookingInterceptor {
+	
+	@PersistenceContext
+	private EntityManager entityManager;
+	
+	/**
+	 * Method stub for further implementation.
+	 */
+	
+	@AroundInvoke
+	public Object aroundInvoke(InvocationContext ctx) {
+		/* PROTECTED REGION ID(java.implementation._17_0_4_2_8210263_1433001489477_281391_3717) ENABLED START */
+		try {
+			System.out.println("Interceptor called");
+			NewBooking newBooking = (NewBooking) ctx.getParameters()[0];
+			Task t = entityManager.find(Task.class, newBooking.getTaskId());
+			Project p = t.getMilestone().getProject();
+			PlanningData latestPlanningData = null;
+			for (PlanningData d : p.getPlanningData()) {
+				if (latestPlanningData == null || latestPlanningData.getTstamp().getTime() < d.getTstamp().getTime()) {
+					latestPlanningData = d;
+				}
+			}
+			float budgetLimit = latestPlanningData.getTimeBudgetPlan();
+			float currentTimeBudgetUsed = p.getTimeBudgetAct();
+			if (currentTimeBudgetUsed + ((newBooking.getEnd().getTime() - newBooking.getStart().getTime()) / 1000 / 60 / 60) > budgetLimit)
+				System.out.println("Time Budget exceeded");
+			else
+				System.out.println("Time Budget okay");
+			try {
+				ctx.proceed();
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+			return null;
+		} finally {
+		}
+		/* PROTECTED REGION END */
+	}
+	
+	/* PROTECTED REGION ID(java.class.own.code.implementation._17_0_4_2_8210263_1433001027643_848303_3640) ENABLED START */
+	// TODO: put your own implementation code here
+	/* PROTECTED REGION END */
 }
